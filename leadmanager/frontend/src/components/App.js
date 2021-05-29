6b78c3821284c01755eebd88ef81023274a8462e
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-29 23:54:24 +0300

    another one

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
-
+import Header from './layout/Header';
 
 class App extends Component {
     render() {
-        return <h1>React App</h1>
+        return (
+            <Header />
+        )
     }
 }
 ReactDOM.render(<App />, document.getElementById('app'));
