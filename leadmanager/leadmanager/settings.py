1e365cb35739c9bbb1f55571de73743c629b71c7
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-29 15:01:23 +0300

    another one

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--fc7+x0=+kdo*bk#-@@ze&d!5q&evkse$oo2vwvvcl2&g6p!ti
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -121,6 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.2/howto/static-files/
 
 STATIC_URL = '/static/'
+
+STATICFILES_DIRS = [
+    BASE_DIR / "static",
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
