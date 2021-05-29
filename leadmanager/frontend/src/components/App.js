209dc549651cebb7445f040bc38bdde275c67de9
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-30 01:29:19 +0300

    another one

--- leadmanager/frontend/src/components/App.js
+++ leadmanager/frontend/src/components/App.js
@@ -1,11 +1,17 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
 import ReactDOM from 'react-dom';
 import Header from './layout/Header';
+import Dashboard from './leads/Dashboard';
 
 class App extends Component {
     render() {
         return (
-            <Header />
+            <Fragment>
+                <Header />
+                <div className="container">
+                    <Dashboard />
+                </div>
+            </Fragment>
         )
     }
 }
