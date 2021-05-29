c3f0ff1fa32993dce274ecfd221a014de96cb6c0
WilliamOtieno <jimmywilliamotieno@gmail.com>
2021-05-29 21:55:01 +0300

    another one

--- leadmanager/leadmanager/settings.py
+++ leadmanager/leadmanager/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -122,9 +123,6 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-STATICFILES_DIRS = [
-    BASE_DIR / "static",
-]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
